384d2d57440aa3924ad66cfa5d1819d095b570c6
Rob Sawyer <robksawyer@gmail.com>
2020-06-11 12:02:54 +0900

    Build updates

--- dist/FileSaver.js
+++ dist/FileSaver.js
@@ -10,7 +10,7 @@
     factory();
     global.FileSaver = mod.exports;
   }
-})(this, function () {
+})(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : this, function () {
   "use strict";
 
   /*
@@ -94,6 +94,7 @@
   /* noop */
   // Use download attribute first if possible (#193 Lumia mobile) unless this is a macOS WebView
   : 'download' in HTMLAnchorElement.prototype && !isMacOSWebView ? function saveAs(blob, name, opts) {
+    console.log('blob, name, opts', blob, name, opts);
     var URL = _global.URL || _global.webkitURL;
     var a = document.createElement('a');
     name = name || blob.name || 'download';
@@ -101,6 +102,8 @@
     a.rel = 'noopener'; // tabnabbing
     // TODO: detect chrome extensions & packaged apps
     // a.target = '_blank'
+
+    console.log('typeof blob === "string"', typeof blob === 'string');
 
     if (typeof blob === 'string') {
       // Support regular links
@@ -156,6 +159,7 @@
     var isSafari = /constructor/i.test(_global.HTMLElement) || _global.safari;
 
     var isChromeIOS = /CriOS\/[\d]+/.test(navigator.userAgent);
+    console.log('isChromeIOS', isChromeIOS);
 
     if ((isChromeIOS || force && isSafari || isMacOSWebView) && typeof FileReader !== 'undefined') {
       // Safari doesn't allow downloading of blob URLs
@@ -164,6 +168,7 @@
       reader.onloadend = function () {
         var url = reader.result;
         url = isChromeIOS ? url : url.replace(/^data:[^;]*;/, 'data:attachment/file;');
+        console.log('final url using FileReader', url);
         if (popup) popup.location.href = url;else location = url;
         popup = null; // reverse-tabnabbing #460
       };
